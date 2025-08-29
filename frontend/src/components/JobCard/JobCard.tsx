807efee9c4100a805d692240f45397b897306786
adriel9-5 <adriel39102248@gmail.com>
2025-08-28 23:42:15 -0300

    Agrego estructura seccion empleos

---
+++ frontend/src/components/JobCard/JobCard.tsx
@@ -0,0 +1,33 @@
+// src/components/JobCard/JobCard.tsx
+import React from "react";
+import styles from "./JobCard.module.css";
+
+interface JobCardProps {
+  title: string;
+  company: string;
+  description: string;
+}
+
+const JobCard: React.FC<JobCardProps> = ({ title, company, description }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <h3 className={styles.title}>{title}</h3>
+        <span className={styles.company}>{company}</span>
+        <p className={styles.description}>{description}</p>
+      </div>
+      
+       <div className={styles.actions}>
+        <button className={styles.applyBtn}>Aplicar</button>
+        <button className={styles.saveBtn}>Guardar</button>
+      </div>
+     
+    </div>
+  );
+};
+
+export default JobCard;
+
+
+
+
--- frontend/src/pages/Empleos.tsx
+++ frontend/src/pages/Empleos.tsx
@@ -1,5 +1,48 @@
-const Empleos = () => {
-  return <div>Empleos</div>;
+import type React from "react";
+import JobCard from "../components/JobCard/JobCard";
+import { useEffect, useState } from "react";
+
+interface Job {
+  id: number,
+  title: string,
+  company: string,
+  description: string 
+}
+
+const Empleos: React.FC = () => {
+  const [jobs, setJobs] = useState<Job[]>([])
+
+  useEffect(()=> {
+    const mockJobs: Job[] = [
+        {
+        id: 1,
+        title: "Frontend Developer",
+        company: "Tech Solutions",
+        description: "Buscamos un Frontend con experiencia en React y TypeScript."
+      },
+      {
+        id: 2,
+        title: "Backend Developer",
+        company: "Cloud Services",
+        description: "Se requiere experiencia en Node.js, Express y SQL."
+      }
+    ];
+    setJobs(mockJobs)
+  }, [])
+
+  return (
+    <div className="">
+      {jobs.map((job) =>(
+        <JobCard key={job.id} title={job.title} company={job.company} description={job.description}/>
+      ))}
+
+    </div>
+  )
+
+
 };
+  
+  
+
 
 export default Empleos;
