94cddb29d49609c62bda1339886381bd85d83002
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2025-09-05 21:22:26 -0300

    Modificación en SecurityConfig_2

--- backend/src/main/java/com/webAppG9/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/webAppG9/backend/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:*", "https://h1-09-n-webapp.vercel.app"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*", "https://h1-09-n-webapp.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Permite enviar cookies si fuera necesario
